ded0147437d0f8345c127efe838e748126c984af
Wilson Brandão <wilsonbrandaocosta@hotmail.com>
2022-12-10 21:42:02 -0300

    connection string e criacao migrations

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using api_agendador_tarefas.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AgendadorTarefasContext>
+(
+    options => options.UseSqlServer
+    (
+        builder.Configuration.GetConnectionString("ConexaoBancoSqlServer")
+    )
+);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
